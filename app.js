83e1dc918a00f774a31e786fea6dd14d6f948907
Ben <ben@agent-cooper.(none)>
2012-06-25 22:43:22 +0100

    connection added, querying and inserting. handshake error

--- app.js
+++ app.js
@@ -4,8 +4,10 @@
  */
 
 var express = require('express')
+  , mysql = require('mysql')
   , routes = require('./routes')
-  , keys = require('../secret/keys.js');
+  , keys = require('../secret/keys.js')
+  , connections = require('../secret/connections.js');
 
 var app = module.exports = express.createServer();
 
@@ -28,7 +30,20 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-var Foursquare = require("node-foursquare")(keys.foursquare);
+// FOURSQUARE **********************************************
+var Foursquare = require("node-foursquare-2")(keys.foursquare);
+// FOURSQUARE **********************************************
+
+// MYSQL ***************************************************
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : connections.mysql.host
+  ,user     : connections.mysql.user
+  ,password : connections.mysql.password
+  ,database : connections.mysql.database
+});
+
+// MYSQL ***************************************************
 
 /************************************************************/
 
@@ -47,6 +62,29 @@ app.get('/callback', function (req, res) {
     }
     else {
       // Save the accessToken and redirect
+      connection.connect();
+            
+      //connection.query('SELECT * FROM users WHERE access_token = '+ connection.escape(accessToken), function(err, rows, fields) {
+        //if (err) {
+		//  throw err;
+		//}else{
+		  //console.log('Query result: ', rows);
+		  //if(rows == 0){
+		    // new user, add them
+			var post = { 'access_token' : accessToken };
+		    console.log('welcome new person');
+		    connection.query("INSERT INTO users SET ?", post, function(err, result) {
+				console.log('inserted?');
+			});
+		  //}else{
+			// old user
+			//console.log('welcome back');
+		  //}
+	    //}
+      //});
+      
+      connection.end();
+      console.log(accessToken);
 	  console.log('redirecting...');
 	  res.writeHead(302, {
 	    'Location': '/'
